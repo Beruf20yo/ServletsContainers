b1eae2af1689bca08a98b317b49ada6f08f34fd1
RinatG <rinat_galimov_1992@mail.ru>
2024-02-29 19:03:28 +0300

    Добавление функционала репозитория

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -2,23 +2,34 @@ package org.example.repository;
 
 import org.example.model.Post;
 
-import java.util.Collections;
+
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PostRepository {
+    CopyOnWriteArrayList<Post> posts = new CopyOnWriteArrayList<>();
     public List<Post> all() {
-        return Collections.emptyList();
+        return posts;
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        return posts.stream().filter(x->x.getId()==id).findAny();
     }
 
     public Post save(Post post) {
+        posts.add(post);
         return post;
     }
 
     public void removeById(long id) {
+        Iterator<Post> iterator = posts.iterator();
+        while (iterator.hasNext()){
+            Post elem = iterator.next();
+            if(elem.getId()==id){
+                posts.remove(elem);
+            }
+        }
     }
 }
